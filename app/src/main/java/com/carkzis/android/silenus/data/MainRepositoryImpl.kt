9f28b8cb824f68852c99af75f461d78b423e5bdd
Carkzis <marc.jowett@gmail.com>
2021-08-24 22:45:00 +0100

    Added flow functions for retrieving reviews

--- app/src/main/java/com/carkzis/android/silenus/data/MainRepositoryImpl.kt
+++ app/src/main/java/com/carkzis/android/silenus/data/MainRepositoryImpl.kt
@@ -9,6 +9,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
@@ -36,6 +37,26 @@ class MainRepositoryImpl (private val firestore: FirebaseFirestore) : MainReposi
         }
 
         emit(LoadingState.Success(R.string.review_added, reviewReference)) // Emit the result!
+
+    }.catch {
+        emit(LoadingState.Error(R.string.error, Exception())) // Emit the error if we get here...
+    }.flowOn(Dispatchers.IO)
+
+    /**
+     * Get the member's reviews, to put into the database.
+     */
+    suspend fun getYourReviews() = flow {
+        val reviews = firestore.collection(getCollectionName(Constants.REVIEWS))
+
+        emit(LoadingState.Loading(R.string.loading)) // Loading!
+
+        val yourReviewList = suspendCoroutine<QuerySnapshot> { cont ->
+            reviews.get()
+                .addOnSuccessListener { cont.resume(it) }
+                .addOnFailureListener { throw Exception() }
+        }.toObjects(YourReview::class.java) // This converts the snapshot into a list.
+
+        emit(LoadingState.Success(R.string.reviews_retrieved, yourReviewList))
 
     }.catch {
         emit(LoadingState.Error(R.string.error, Exception())) // Emit the error if we get here...
